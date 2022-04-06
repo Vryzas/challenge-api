9bdb3d102d419a7fbe9f9de7b74e4b3e2ba21f32
Vitor Costa <vitor.costa@exaud.com>
2022-04-06 14:54:59 +0100

    CA-1 - updated gitignore, singlequotes instead of double
    rebased the base branch
    
    git rebase --continue
    :wq

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const compression = require("compression");
-const cors = require("cors");
-const sequelize = require("./utils/dbconnection");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+const sequelize = require('./utils/dbconnection');
 // Start express app
 const app = express();
 
-app.enable("trust proxy");
+app.enable('trust proxy');
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 // Implement CORS
 app.use(cors());
 
-app.options("*", cors());
+app.options('*', cors());
 
 // Serving static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 app.use(compression());
@@ -39,13 +39,13 @@ app.use(compression());
 (async function () {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error('Unable to connect to the database:', error);
   }
 })();
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
