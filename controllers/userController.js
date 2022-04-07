2f6f86d0c43619ab3b42d23feda997e6eb27ceda
Vitor Costa <vitor.costa@exaud.com>
2022-04-07 11:58:27 +0100

    CA-1 - added userController methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,17 @@
-// TODO: user controller
+exports.getMe = async function (req, res, next) {
+  return res
+    .status(501)
+    .json({ message: 'Get my data is still to be impemented.' });
+};
+
+exports.getMyStats = async function (req, res, next) {
+  return res
+    .status(501)
+    .json({ message: 'Get my statistics is still to be impemented.' });
+};
+
+exports.getMyMatches = async function (req, res, next) {
+  return res
+    .status(501)
+    .json({ message: 'Get my matches is still to be impemented.' });
+};
