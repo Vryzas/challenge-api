2f8eb3cab289b39099f55a646cdfbdabe63971a8
Vitor Costa <vitor.costa@exaud.com>
2022-04-07 11:21:49 +0100

    CA-1 - added user Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,16 @@
-// TODO: user routes
+const express = require('express');
+
+const authController = require('./../controllers/authController');
+const userController = require('./../controllers/userController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+
+router.get('/getMe', userController.getMe);
+router.get('/getMyStats', userController.getMyStats);
+router.get('/getMyMatches', userController.getMyMatches);
+
+module.exports = router;
