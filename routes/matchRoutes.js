2c8add9dd7202a13e2efb14a1ca98752d037fc2b
Vitor Costa <vitor.costa@exaud.com>
2022-04-07 16:02:22 +0100

    CA-1 - added match routes

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -1,1 +1,12 @@
-// TODO: matches routes
+const express = require('express');
+
+const matchController = require('./../controllers/matchController');
+
+const router = express.Router();
+
+router.get('/playerMatches/:username', matchController.playerMatches);
+router.get('/compareMatches/:compareTo', matchController.compareMatches);
+
+router.post('/addMatch', matchController.addMatch);
+
+module.exports = router;
