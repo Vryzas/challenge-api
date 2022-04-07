b0d3c9a874ff38442e6af82aeba66c8b845cb2e9
Vitor Costa <vitor.costa@exaud.com>
2022-04-07 15:34:46 +0100

    CA-1 - added stats controller methods

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,1 +1,35 @@
-// TODO: stats controller
+exports.winUpdate = async (req, res, next) => {
+  const winner = req.params.username;
+  return res.status(501).json({
+    message: 'Updating user victories is still to be impemented.' + `${winner}`,
+  });
+};
+
+exports.defeatUpdate = async (req, res, next) => {
+  const loser = req.params.username;
+  return res.status(501).json({
+    message: 'Updating user defeats is still to be impemented.' + `${loser}`,
+  });
+};
+
+exports.drawUpdate = async (req, res, next) => {
+  const draw = req.params.username;
+  return res
+    .status(501)
+    .json({ message: 'Updating draws is still to be impemented.' + `${draw}` });
+};
+
+exports.findStats = async (req, res, next) => {
+  const username = req.params.username;
+  return res.status(501).json({
+    message: `Getting the stats of ${username} is still to be impemented.`,
+  });
+};
+
+exports.compareStats = async (req, res, next) => {
+  const toCompare = req.body.toCompare;
+  const compareTo = req.body.compareTo;
+  return res.status(501).json({
+    message: `Comparing stats with between ${toCompare} and ${compareTo}user is still to be impemented.`,
+  });
+};
