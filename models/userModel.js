773e08f74757e686e4417ed3fa58c1a04379f860
Vitor Costa <vitor.costa@exaud.com>
2022-04-07 10:27:59 +0100

    CA-1 - added userModel/table template

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,36 @@
-// TODO: userModel
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('./../utils/dbconnection');
+
+class User extends Model {}
+
+User.init(
+  {
+    // id: {
+    //   type: DataTypes.INTEGER,
+    //   autoIncrement: true,
+    //   primaryKey: true,
+    // },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    timestamps: false,
+  }
+);
+
+User.sync({ alter: true });
+console.log(User === sequelize.models.User);
+module.exports = User;
