21ea0f599e533a09f3aac1e9cea148c5b78a33a2
Vitor Costa <vitor.costa@exaud.com>
2022-04-12 12:29:18 +0100

    CA-2 - created user method added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,19 @@
-exports.signup = async function (req, res, next) {
-  return res.status(501).json({ message: 'Signup is still to be impemented.' });
+const User = require('./../models/userModel');
+
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json({
+      message:
+        'Your profile has been created, please check your email for the activation message.',
+    });
+  } catch (err) {
+    return res.status(500).json({ message: 'Something went wrong!' });
+  }
 };
 
 exports.login = async function (req, res, next) {
