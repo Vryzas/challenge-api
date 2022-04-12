25eea06966ec9c4c488ce1d2a268a4aa5b72a3db
Vitor Costa <vitor.costa@exaud.com>
2022-04-12 12:29:18 +0100

    CA-2 - added login method

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,8 +23,27 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.login = async function (req, res, next) {
-  return res.status(501).json({ message: 'Login is still to be impemented.' });
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Please provide username and password!' });
+    }
+
+    const user = await User.findByPk(username);
+    if (!user || password !== user.password) {
+      return res.status(401).json({ message: 'Wrong username or password!' });
+    }
+
+    return res.status(200).json({
+      message: 'Login successful.',
+      data: { username: user.username, email: user.email },
+    });
+  } catch (err) {
+    return res.status(500).json({ message: 'Something went wrong!' });
+  }
 };
 
 exports.logout = async function (req, res, next) {
