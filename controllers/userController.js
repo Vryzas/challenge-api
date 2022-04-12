fd134c4c2fe842ba29f64e9ca627f44def284231
Vitor Costa <vitor.costa@exaud.com>
2022-04-12 16:06:35 +0100

    CA-2 - added user activation method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('./../models/userModel');
+
 exports.getMe = async function (req, res, next) {
   return res
     .status(501)
@@ -14,4 +16,26 @@ exports.getMyMatches = async function (req, res, next) {
   return res
     .status(501)
     .json({ message: 'Get my matches is still to be impemented.' });
+};
+
+exports.activateAccount = async (req, res, next) => {
+  // check if user exists and is already activated
+  const user = await User.findByPk(req.params.username);
+  if (!user || user.active) {
+    return res
+      .status(400)
+      .json({ message: 'Wrong username or user is already activated!' });
+  }
+  try {
+    user.active = true;
+    if (await user.save()) {
+      return res
+        .status(200)
+        .json({ message: 'Your account has been activated successfully.' });
+    }
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: 'Something went wrong with your activation!' });
+  }
 };
