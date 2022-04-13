57eb33ab563e8314d53a1efb0851c7af35b999d2
Vitor Costa <vitor.costa@exaud.com>
2022-04-13 16:48:06 +0100

    CA-2 - added password reseting routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,9 @@ router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
 router.patch('/activateMe/:username', userController.activateAccount);
-
+router.patch('/forgotPassword', userController.forgotPassword);
+router.get('/resetPassword/:token', userController.resetPassword);
+router.patch('/passwordRedefined/:username', userController.passwordRedefined);
 router.get('/getMe', userController.getMe);
 router.get('/getMyStats', userController.getMyStats);
 router.get('/getMyMatches', userController.getMyMatches);
