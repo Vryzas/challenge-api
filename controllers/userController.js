b43f66a67265293ce18ce214af9de1fa0129f69e
Vitor Costa <vitor.costa@exaud.com>
2022-04-13 16:45:09 +0100

    CA-2 - added forgotPassword method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require('./../models/userModel');
+const sendEmail = require('./../utils/email');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 exports.getMe = async function (req, res, next) {
   return res
@@ -37,5 +40,39 @@ exports.activateAccount = async (req, res, next) => {
     return res
       .status(400)
       .json({ message: 'Something went wrong with your activation!' });
+  }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  const email = req.body.email;
+  const user = await User.findOne({ email: email });
+  if (!user || !user.active) {
+    return res
+      .status(404)
+      .json({ message: 'No user with that email or user is not active!' });
+  }
+  try {
+    const key = user.username;
+    const token = jwt.sign({ key }, process.env.JWT_SECRET);
+    const url = `${req.protocol}://${req.get('host')}/resetPassword/${token}`;
+    user.passwordResetToken = token;
+    user.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
+    if (await user.save()) {
+      await sendEmail({
+        email: user.email,
+        subject: 'Password reset',
+        message: `To reset your password, please click the link within the next 10 minutes.
+        \n${url}`,
+      });
+      return res.status(200).json({
+        message:
+          'Email has been sent. Check your inbox for password recovery instructions.',
+      });
+    }
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.ppasswordResetExpires = undefined;
+    await user.save();
+    return res.status(500).json({ message: 'Something went wrong!' });
   }
 };
