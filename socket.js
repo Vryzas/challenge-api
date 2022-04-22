003e125e7a3be829a503ef4de7c3cb056c3f473d
Vitor Costa <vitor.costa@exaud.com>
2022-04-22 17:04:07 +0100

    CA-3 - handled connection/disconnection

--- socket.js
+++ socket.js
@@ -6,25 +6,31 @@ const io = socketIO(server);
 
 const User = require('././/models/userModel');
 
+let usersOnline = [];
+
 io.use(async (socket, next) => {
   const user = await User.findByPk(socket.handshake.headers.user);
-  console.log;
   if (user === null || !user.logedIn) {
-    next(new Error('invalid'));
+    next(new Error(`Invalid credentials or not loged in!`));
   } else {
+    usersOnline.push([user.username, socket]);
     next();
   }
 });
 
 io.on('connection', (socket) => {
-  console.log(
-    'Socket connection established!',
-    socket.handshake.headers.user,
-    socket.id
-  );
+  const user = socket.handshake.headers.user;
+  socket.broadcast.emit('onConnect', `${user} is online!`);
+
+  socket.on('disconnect', (socket) => {
+    console.log(`${user} has disconnected!`);
+    io.emit('onDisconnect', `${user} disconnected!`);
+    usersOnline = usersOnline.filter((val) => val[0] !== user);
+    usersOnline.forEach((val) => console.log(val[0]));
+  });
 });
+
 // TODO:
-// Handle connection and disconnection to the socket from Users
 // Send Challenge Request to another User
 // Answer with an error if User challenged is not available
 // Answer with Request response when other User handles Request
