4529803c1c2388c496655a8c9c03ff69f4682184
Vitor Costa <vryzas27@gmail.com>
2022-04-28 15:47:19 +0100

    CA-2 - added user verification to routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,16 +6,16 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/login', userController.activeUser, authController.login);
 router.get('/logout', authController.logout);
 
 router.patch('/activate/:username', userController.activate);
-router.patch('/forgotPassword', userController.forgotPassword);
+router.patch('/forgotPassword', userController.activeUser, userController.forgotPassword);
 router.get('/resetPassword/:token', userController.resetPassword);
 router.patch('/changePassword/:username', userController.changePassword);
 
-router.get('/getMe', userController.getMe);
-router.get('/getMyStats', userController.getMyStats);
-router.get('/getMyMatches', userController.getMyMatches);
+router.get('/getMe', userController.activeUser, userController.getMe);
+router.get('/getMyStats', userController.activeUser, userController.getMyStats);
+router.get('/getMyMatches', userController.activeUser, userController.getMyMatches);
 
 module.exports = router;
