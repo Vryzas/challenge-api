04713976301b4e65916a7e1f12693b972f36a297
Vitor Costa <vryzas27@gmail.com>
2022-04-29 11:28:03 +0100

    CA-5 - fixed login route (get, not post)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.get('/login', authController.login);
 router.get('/logout/:username', authController.logout);
 
 router.patch('/activateMe/:username', userController.activateAccount);
