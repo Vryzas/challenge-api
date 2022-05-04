98efc18d66dfa56bcc14b4bc38d6627659ac30d0
Vitor Costa <vryzas27@gmail.com>
2022-05-04 12:25:18 +0100

    CA-6 - added catchAsync to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,28 @@
+const catchAsync = require('./../utils/catchAsync');
 const User = require('./../models/userModel');
 const sendEmail = require('./../utils/email');
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
-exports.getMe = async function (req, res, next) {
+exports.getMe = catchAsync(async function (req, res, next) {
   return res
     .status(501)
     .json({ message: 'Get my data is still to be impemented.' });
-};
+});
 
-exports.getMyStats = async function (req, res, next) {
+exports.getMyStats = catchAsync(async function (req, res, next) {
   return res
     .status(501)
     .json({ message: 'Get my statistics is still to be impemented.' });
-};
+});
 
-exports.getMyMatches = async function (req, res, next) {
+exports.getMyMatches = catchAsync(async function (req, res, next) {
   return res
     .status(501)
     .json({ message: 'Get my matches is still to be impemented.' });
-};
+});
 
-exports.activateAccount = async (req, res, next) => {
+exports.activateAccount = catchAsync(async (req, res, next) => {
   // check if user exists and is already activated
   const user = await User.findByPk(req.params.username);
   if (!user || user.active) {
@@ -29,21 +30,15 @@ exports.activateAccount = async (req, res, next) => {
       .status(400)
       .json({ message: 'Wrong username or user is already activated!' });
   }
-  try {
-    user.active = true;
-    if (await user.save()) {
-      return res
-        .status(200)
-        .json({ message: 'Your account has been activated successfully.' });
-    }
-  } catch (err) {
+  user.active = true;
+  if (await user.save()) {
     return res
-      .status(400)
-      .json({ message: 'Something went wrong with your activation!' });
+      .status(200)
+      .json({ message: 'Your account has been activated successfully.' });
   }
-};
+});
 
-exports.forgotPassword = async (req, res, next) => {
+exports.forgotPassword = catchAsync(async (req, res, next) => {
   const email = req.body.email;
   const user = await User.findOne({ email: email });
   if (!user || !user.active) {
@@ -51,69 +46,53 @@ exports.forgotPassword = async (req, res, next) => {
       .status(404)
       .json({ message: 'No user with that email or user is not active!' });
   }
-  try {
-    const key = user.username;
-    const token = jwt.sign({ key }, process.env.JWT_SECRET);
-    const url = `${req.protocol}://${req.get('host')}/resetPassword/${token}`;
-    user.passwordResetToken = token;
-    user.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
-    if (await user.save()) {
-      await sendEmail({
-        email: user.email,
-        subject: 'Password reset',
-        message: `To reset your password, please click the link within the next 10 minutes.
+  const key = user.username;
+  const token = jwt.sign({ key }, process.env.JWT_SECRET);
+  const url = `${req.protocol}://${req.get('host')}/resetPassword/${token}`;
+  user.passwordResetToken = token;
+  user.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
+  if (await user.save()) {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset',
+      message: `To reset your password, please click the link within the next 10 minutes.
         \n${url}`,
-      });
-      return res.status(200).json({
-        message:
-          'Email has been sent. Check your inbox for password recovery instructions.',
-      });
-    }
-  } catch (err) {
-    user.passwordResetToken = undefined;
-    user.ppasswordResetExpires = undefined;
-    await user.save();
-    return res.status(500).json({ message: 'Something went wrong!' });
-  }
-};
-
-exports.resetPassword = async function (req, res, next) {
-  const user = await User.findOne({ passwordResetToken: req.params.token });
-  try {
-    if (user && new Date(+Date.now()) < +user.passwordResetExpires) {
-      return res.status(302).json({
-        message: 'Please insert a new password in the highlighted field',
-      });
-    }
-    if (!user || new Date(+Date.now()) > +user.passwordResetExpires) {
-      return res
-        .status(403)
-        .json({ message: 'Your password reset link has expired!' });
-    }
-  } catch (err) {
-    return res.status(501).json({ message: 'Something went wrong!' });
-  }
-};
-
-exports.passwordRedefined = async (req, res, next) => {
-  const user = await User.findByPk(req.params.username);
-  try {
-    if (!user.passwordResetToken || !user.passwordResetExpires) {
-      return res.status(403).json({
-        message: `You don't have a password reset request!`,
-      });
-    }
-    user.password = req.body.newPassword;
-    user.passwordResetToken = null;
-    user.passwordResetExpires = null;
-    if (user.save()) {
-      return res.status(200).json({
-        message: `${req.params.username}, your password has been redefined successfully.`,
-      });
-    }
-  } catch (err) {
-    return res.status(501).json({
-      message: `Something went wrong! \nYour password hasn't changed!`,
+    });
+    return res.status(200).json({
+      message:
+        'Email has been sent. Check your inbox for password recovery instructions.',
     });
   }
-};
+});
+
+exports.resetPassword = catchAsync(async function (req, res, next) {
+  const user = await User.findOne({ passwordResetToken: req.params.token });
+
+  if (user && new Date(+Date.now()) < +user.passwordResetExpires) {
+    return res.status(302).json({
+      message: 'Please insert a new password in the highlighted field',
+    });
+  }
+  if (!user || new Date(+Date.now()) > +user.passwordResetExpires) {
+    return res
+      .status(403)
+      .json({ message: 'Your password reset link has expired!' });
+  }
+});
+
+exports.passwordRedefined = catchAsync(async (req, res, next) => {
+  const user = await User.findByPk(req.params.username);
+  if (!user.passwordResetToken || !user.passwordResetExpires) {
+    return res.status(403).json({
+      message: `You don't have a password reset request!`,
+    });
+  }
+  user.password = req.body.newPassword;
+  user.passwordResetToken = null;
+  user.passwordResetExpires = null;
+  if (user.save()) {
+    return res.status(200).json({
+      message: `${req.params.username}, your password has been redefined successfully.`,
+    });
+  }
+});
