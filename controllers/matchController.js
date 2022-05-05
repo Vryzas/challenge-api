7d300ea1d6ebee86f6d4be5ebea7cd20f9c3a77f
Vitor Costa <vryzas27@gmail.com>
2022-05-05 15:00:22 +0100

    CA-8 - added newMatch method

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -1,4 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const Match = require('./../models/matchModel');
 
 exports.playerMatches = catchAsync(async function (req, res, next) {
   const player = req.params.username;
@@ -16,12 +18,17 @@ exports.compareMatches = catchAsync(async function (req, res, next) {
 });
 
 exports.addMatch = catchAsync(async function (req, res, next) {
-  const game = req.body.game;
-  const username1 = req.body.username1;
-  const username2 = req.body.username2;
-  const score1 = req.body.score1;
-  const score2 = req.body.score2;
-  return res.status(501).json({
-    message: `Add a ${game} match between ${username1} and ${username2} is still to be implemented`,
+  if (req.body.username1 === req.body.username2) {
+    return next(new AppError(`Save game fail! Same username on both players!?`, 400));
+  }
+  const newGame = await Match.create({
+    game: req.body.game,
+    username1: req.body.username1,
+    username2: req.body.username2,
+    score1: req.body.score1,
+    score2: req.body.score2,
+  });
+  return res.status(201).json({
+    message: `Game saved successfully.`,
   });
 });
