e51de44dc304006ce5592a21effddd1849fe15da
Vitor Costa <vryzas27@gmail.com>
2022-05-05 15:05:52 +0100

    CA-8 - changed matches compare route

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -5,7 +5,7 @@ const matchController = require('./../controllers/matchController');
 const router = express.Router();
 
 router.get('/playerMatches/:username', matchController.playerMatches);
-router.get('/compareMatches/:compareTo', matchController.compareMatches);
+router.get('/compareMatches', matchController.compareMatches);
 
 router.post('/addMatch', matchController.addMatch);
 
