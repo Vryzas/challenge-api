4a2000fb2a4ce7b6a7aadffe7fc216c841636435
Vitor Costa <vryzas27@gmail.com>
2022-05-06 11:01:13 +0100

    CA-8 - added matches compare method

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -3,6 +3,20 @@ const AppError = require('./../utils/appError');
 const Match = require('./../models/matchModel');
 const User = require(`./../models/userModel`);
 const { Op } = require('sequelize');
+const lodash = require('lodash');
+
+const getTotalScore = (games, user) => {
+  const score = lodash.sum(
+    games.map((val) => {
+      if (user === val.username1) {
+        return val.score1;
+      } else {
+        return val.score2;
+      }
+    })
+  );
+  return score;
+};
 
 exports.playerMatches = catchAsync(async function (req, res, next) {
   const player = await User.findByPk(req.params.username);
@@ -21,10 +35,25 @@ exports.playerMatches = catchAsync(async function (req, res, next) {
 });
 
 exports.compareMatches = catchAsync(async function (req, res, next) {
-  const compareTo = req.params.compareTo;
-  const me = req.body.username;
-  return res.status(501).json({
-    message: `Compare matches between ${me} and ${compareTo} is still to be impemented.`,
+  const gamesUser = await Match.findAll({
+    where: {
+      [Op.or]: [
+        { username1: req.body.username1, username2: req.body.username2 },
+        { username1: req.body.username2, username2: req.body.username1 },
+      ],
+    },
+  });
+  if (!gamesUser.length >= 1) {
+    return res.status(404).json({ message: `No games found between these players!` });
+  }
+  const scored1 = getTotalScore(gamesUser, req.body.username1);
+  const scored2 = getTotalScore(gamesUser, req.body.username2);
+
+  return res.status(200).json({
+    gamesPlayed: gamesUser.length,
+    user1: scored1,
+    user2: scored2,
+    games: gamesUser,
   });
 });
 
