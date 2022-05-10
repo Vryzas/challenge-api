4f874c5d422cf7be31a85f56ac10492e90722c1b
Vitor Costa <vryzas27@gmail.com>
2022-05-10 11:24:29 +0100

    CA-2 -  corrected routes order

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,13 +9,13 @@ router.post('/signup', authController.signup);
 router.post('/login', userController.activeUser, authController.login);
 router.get('/logout', authController.logout);
 
+router.get('/getMe', userController.activeUser, userController.getMe);
+router.get('/getMyStats', userController.activeUser, userController.getMyStats);
+router.get('/getMyMatches', userController.activeUser, userController.getMyMatches);
+
 router.patch('/activate/:username', userController.activate);
 router.patch('/forgotPassword', userController.activeUser, userController.forgotPassword);
 router.get('/resetPassword/:token', userController.resetPassword);
 router.patch('/changePassword/:username', userController.changePassword);
-
-router.get('/getMe', userController.activeUser, userController.getMe);
-router.get('/getMyStats', userController.activeUser, userController.getMyStats);
-router.get('/getMyMatches', userController.activeUser, userController.getMyMatches);
 
 module.exports = router;
