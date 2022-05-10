78c6b8c2dd679cf394394aac9ab5eb4dce96ed6f
Vitor Costa <vryzas27@gmail.com>
2022-05-10 12:12:57 +0100

    CA-2 - fixed validation on userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,19 +2,20 @@ const express = require('express');
 
 const authController = require('./../controllers/authController');
 const userController = require('./../controllers/userController');
+const validation = require('./../utils/validation');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-router.post('/login', userController.activeUser, authController.login);
+router.post('/login', validation.activeUser, authController.login);
 router.get('/logout', authController.logout);
 
-router.get('/getMe', userController.activeUser, userController.getMe);
-router.get('/getMyStats', userController.activeUser, userController.getMyStats);
-router.get('/getMyMatches', userController.activeUser, userController.getMyMatches);
+router.get('/getMe', validation.activeUser, userController.getMe);
+router.get('/getMyStats', validation.activeUser, userController.getMyStats);
+router.get('/getMyMatches', validation.activeUser, userController.getMyMatches);
 
 router.patch('/activate/:username', userController.activate);
-router.patch('/forgotPassword', userController.activeUser, userController.forgotPassword);
+router.patch('/forgotPassword', validation.activeUser, userController.forgotPassword);
 router.get('/resetPassword/:token', userController.resetPassword);
 router.patch('/changePassword/:username', userController.changePassword);
 
