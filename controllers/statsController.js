7b86ef9d9e1bdf7de10033040d5011f399e80cee
Vitor Costa <vryzas27@gmail.com>
2022-05-12 16:56:43 +0100

    CA-7 - fixed stats compare

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -12,17 +12,32 @@ exports.findStats = catchAsync(async (req, res, next) => {
 });
 
 exports.compareStats = catchAsync(async (req, res, next) => {
-  const usernames = [
-    req.body.username1,
-    req.body.username2,
-    req.body.username3,
-    req.body.username4,
-  ];
+  const usernames = [req.query.username1, req.query.username2, req.query.username3, req.query.username4].filter(
+    (val) => {
+      if (val !== undefined) return val;
+    }
+  );
+  if (usernames.length <= 1) {
+    return res.status(400).json({
+      message: 'You need at least 2 players to compare!',
+    });
+  }
   const stats = await Stat.findAll({
     where: {
       username: usernames,
     },
   });
+  if (stats.length === 0) {
+    return res.status(404).json({
+      message: 'No stats found for the players names inserted!',
+    });
+  }
+  if (stats.length === 1) {
+    return res.status(206).json({
+      alert: `Can't compare! Only found the statistics from ${stats[0].dataValues.username}!`,
+      message: stats,
+    });
+  }
   return res.status(200).json({
     message: stats,
   });
