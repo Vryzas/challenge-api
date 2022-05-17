b6ab9ad10f65abf448e79cb3b96367b5ef74c217
Vitor Costa <vryzas27@gmail.com>
2022-05-17 15:19:54 +0100

    CA-5 - refactored appError class

--- utils/appError.js
+++ utils/appError.js
@@ -1,9 +1,8 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  constructor(status, message) {
     super(message);
 
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.status = status;
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
