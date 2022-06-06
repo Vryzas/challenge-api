aa675af4dbcd38f6c1aa9d16112bcd370b48a0b2
Vitor Costa <vryzas27@gmail.com>
2022-06-06 11:35:02 +0100

    CA-6 - created Validations file/signup validation

---
+++ utils/validation.js
@@ -0,0 +1,31 @@
+const emailVal = (email) => {
+  const format =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email.match(format);
+};
+
+exports.signupFields = (req, res, next) => {
+  let ret = {
+    message: ``,
+    status: 200,
+  };
+  if (!req.body.username) {
+    ret.message = `No username provided!`;
+    ret.status = 400;
+  } else if (!req.body.email) {
+    ret.message = 'No email provided!';
+    ret.status = 400;
+  } else if (!emailVal(req.body.email)) {
+    ret.message = 'Invalid email!';
+    ret.status = 400;
+  } else if (!req.body.password) {
+    ret.message = 'No password provided!';
+    ret.status = 400;
+  }
+
+  if (ret.status === 200) {
+    next();
+  } else {
+    res.status(ret.status).json({ message: ret.message });
+  }
+};
