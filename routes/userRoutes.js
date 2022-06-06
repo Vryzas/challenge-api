c42dd4ea3866cfc70437f94f1e16fcd7ab1967f0
Vitor Costa <vryzas27@gmail.com>
2022-06-06 11:37:36 +0100

    CA-6 - added validations to userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const authController = require('./../controllers/authController');
 const userController = require('./../controllers/userController');
+const validation = require('./../utils/validation');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.get('/login', authController.login);
+router.post('/signup', validation.signupFields, authController.signup);
+router.get('/login', validation.loginFields, authController.login);
 router.get('/logout/:username', authController.logout);
 
 router.patch('/activateMe/:username', userController.activateAccount);
