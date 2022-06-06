aeb6bac5672047fde6fcd036ddb0d97c0ee618eb
Vitor Costa <vryzas27@gmail.com>
2022-06-06 13:14:56 +0100

    CA-6 - refactored chessController stadard response

--- controllers/chessController.js
+++ controllers/chessController.js
@@ -1,44 +1,46 @@
 const catchAsync = require('./../utils/catchAsync');
 const axios = require('axios');
+const errorController = require('./../controllers/errorController');
+const AppError = require('./../utils/appError');
 
 exports.chessPlayer = catchAsync(async (req, res, next) => {
   const player = req.body.player;
   const url = `https://api.chess.com/pub/player/${player}/stats`;
-  let status;
-  let message;
   await axios
     .get(url)
     .then((response) => {
-      status = response.status;
-      message = response.data.fide || 'No data about that player found!';
+      if (response.data.fide) {
+        return res.status(200).json({
+          message: response.data.fide,
+        });
+      } else {
+        // if a player exists but has no fide score
+        return errorController(new AppError('No FIDE score for that player found!', 404), res);
+      }
     })
     .catch((error) => {
-      status = error.response.status;
-      message = error.response.data.message || 'No player with that id found!';
+      return errorController(new AppError('No player with that id found!', 404), res);
     });
-  return await res.status(status).json({
-    message: message,
-  });
 });
 
 exports.chessMatchesByPlayer = catchAsync(async (req, res, next) => {
   const player = req.body.player;
-  const url = `https://api.chess.com/pub/player/${player}/games/2022/04`;
+  const url = `https://api.chess.com/pub/player/${player}/games/2022/06`;
   let status;
   let message;
   await axios
     .get(url)
     .then((response) => {
-      status = response.status;
-      message = {
-        games: response.data.games.length || 'No games from that player found!',
-      };
+      if (response.data.games.length > 0) {
+        return res.status(200).json({
+          message: response.data.games.length,
+        });
+      } else {
+        // if player has no games played in that period
+        return errorController(new AppError('No games found for that player!', 404), res);
+      }
     })
     .catch((error) => {
-      status = error.response.status;
-      message = error.response.data.message || 'No player with that id found!';
+      return errorController(new AppError('No player with that id found!', 404), res);
     });
-  return await res.status(status).json({
-    message: message,
-  });
 });
