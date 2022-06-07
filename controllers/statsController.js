93c56f959bd10212edb2f03a3d4ef1f3b596235c
Vitor Costa <vryzas27@gmail.com>
2022-06-07 15:47:25 +0100

    CA-8 - refactored message in findStats

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -4,7 +4,9 @@ const Stat = require('./../models/statsModel');
 exports.findStats = catchAsync(async (req, res, next) => {
   const userStats = await Stat.findByPk(req.params.username);
   if (!userStats) {
-    return res.status(404).json({ message: `No player with that name found!` });
+    return res.status(404).json({
+      message: `No player with username ${req.params.username} found!`,
+    });
   }
   return res.status(200).json({
     message: userStats,
@@ -12,12 +14,7 @@ exports.findStats = catchAsync(async (req, res, next) => {
 });
 
 exports.compareStats = catchAsync(async (req, res, next) => {
-  const usernames = [
-    req.body.username1,
-    req.body.username2,
-    req.body.username3,
-    req.body.username4,
-  ];
+  const usernames = [req.body.username1, req.body.username2, req.body.username3, req.body.username4];
   const stats = await Stat.findAll({
     where: {
       username: usernames,
