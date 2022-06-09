0aa9b04614b8fb61257b261095e3d13be71dc1af
Vitor Costa <vryzas27@gmail.com>
2022-06-09 13:01:03 +0100

    CA-6 - removed returns from statsController

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -2,28 +2,26 @@ const catchAsync = require('./../utils/catchAsync');
 
 exports.winUpdate = catchAsync(async (req, res, next) => {
   const winner = req.params.username;
-  return res.status(501).json({
+  res.status(501).json({
     message: 'Updating user victories is still to be impemented.' + `${winner}`,
   });
 });
 
 exports.defeatUpdate = catchAsync(async (req, res, next) => {
   const loser = req.params.username;
-  return res.status(501).json({
+  res.status(501).json({
     message: 'Updating user defeats is still to be impemented.' + `${loser}`,
   });
 });
 
 exports.drawUpdate = catchAsync(async (req, res, next) => {
   const draw = req.params.username;
-  return res
-    .status(501)
-    .json({ message: 'Updating draws is still to be impemented.' + `${draw}` });
+  res.status(501).json({ message: 'Updating draws is still to be impemented.' + `${draw}` });
 });
 
 exports.findStats = catchAsync(async (req, res, next) => {
   const username = req.params.username;
-  return res.status(501).json({
+  res.status(501).json({
     message: `Getting the stats of ${username} is still to be impemented.`,
   });
 });
@@ -31,7 +29,7 @@ exports.findStats = catchAsync(async (req, res, next) => {
 exports.compareStats = catchAsync(async (req, res, next) => {
   const toCompare = req.body.toCompare;
   const compareTo = req.body.compareTo;
-  return res.status(501).json({
+  res.status(501).json({
     message: `Comparing stats with between ${toCompare} and ${compareTo}user is still to be impemented.`,
   });
 });
