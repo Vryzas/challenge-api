dd8700dc825fe25291e02990bb511f00c842c740
Vitor Costa <vryzas27@gmail.com>
2022-06-09 12:34:59 +0100

    CA-6 - moved success responses to final condition

--- controllers/chessController.js
+++ controllers/chessController.js
@@ -9,13 +9,13 @@ exports.chessPlayer = catchAsync(async (req, res, next) => {
   await axios
     .get(url)
     .then((response) => {
-      if (response.data.fide) {
+      if (!response.data.fide) {
+        // if a player exists but has no fide score
+        errorController(new AppError('No FIDE score for that player found!', 404), res);
+      } else {
         res.status(200).json({
           message: response.data.fide,
         });
-      } else {
-        // if a player exists but has no fide score
-        errorController(new AppError('No FIDE score for that player found!', 404), res);
       }
     })
     .catch((error) => {
@@ -31,13 +31,13 @@ exports.chessMatchesByPlayer = catchAsync(async (req, res, next) => {
   await axios
     .get(url)
     .then((response) => {
-      if (response.data.games.length > 0) {
+      if (response.data.games.length <= 0) {
+        // if player has no games played in that period
+        errorController(new AppError('No games found for that player!', 404), res);
+      } else {
         res.status(200).json({
           message: response.data.games.length,
         });
-      } else {
-        // if player has no games played in that period
-        errorController(new AppError('No games found for that player!', 404), res);
       }
     })
     .catch((error) => {
