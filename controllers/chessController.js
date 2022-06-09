ae46d96bc4469ea11c36882f1aea6f13a9177b32
Vitor Costa <vryzas27@gmail.com>
2022-06-09 11:49:18 +0100

    CA-6 - removed  'return' in chessControlller

--- controllers/chessController.js
+++ controllers/chessController.js
@@ -10,16 +10,16 @@ exports.chessPlayer = catchAsync(async (req, res, next) => {
     .get(url)
     .then((response) => {
       if (response.data.fide) {
-        return res.status(200).json({
+        res.status(200).json({
           message: response.data.fide,
         });
       } else {
         // if a player exists but has no fide score
-        return errorController(new AppError('No FIDE score for that player found!', 404), res);
+        errorController(new AppError('No FIDE score for that player found!', 404), res);
       }
     })
     .catch((error) => {
-      return errorController(new AppError('No player with that id found!', 404), res);
+      errorController(new AppError('No player with that id found!', 404), res);
     });
 });
 
@@ -32,15 +32,15 @@ exports.chessMatchesByPlayer = catchAsync(async (req, res, next) => {
     .get(url)
     .then((response) => {
       if (response.data.games.length > 0) {
-        return res.status(200).json({
+        res.status(200).json({
           message: response.data.games.length,
         });
       } else {
         // if player has no games played in that period
-        return errorController(new AppError('No games found for that player!', 404), res);
+        errorController(new AppError('No games found for that player!', 404), res);
       }
     })
     .catch((error) => {
-      return errorController(new AppError('No player with that id found!', 404), res);
+      errorController(new AppError('No player with that id found!', 404), res);
     });
 });
