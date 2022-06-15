81016b2bd4a28603f63036c7cbb2ce8680e2ab3b
Vitor Costa <vryzas27@gmail.com>
2022-06-15 17:01:32 +0100

    CA-9 - added signup tests

--- __tests__/controllers/authController.test.js
+++ __tests__/controllers/authController.test.js
@@ -8,3 +8,32 @@ jest.mock('../../utils/appError');
 jest.mock('../../utils/email');
 
 jest.mock('../../dao/userDao', () => (dao = { create: jest.fn(), findUser: jest.fn(), save: jest.fn() }));
+afterEach(() => {
+  // clear the responses after each test (else they accumulate)
+  clearMockRes();
+});
+
+describe('Signup', () => {
+  test('No parameters.', async () => {
+    // arrange
+    const req = getMockReq();
+    dao.create.mockRejectedValue('NullValuesError');
+    // act
+    authController.signup(req, res, next);
+    // assert
+    expect(dao.create).rejects.toMatch('NullValuesError');
+  });
+
+  test('Valid parameters.', async () => {
+    // arrange
+    const req = getMockReq({ body: { username: 'username', email: 'email', password: 'password', token: 'token' } });
+    dao.create.mockReturnValue(true);
+    // act
+    await authController.signup(req, res, next);
+    // assert
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Your profile has been created, please check your email for the activation message.',
+    });
+  });
+});
