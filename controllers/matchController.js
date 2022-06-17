2ea3bb8f3c9567d97406343fd685a752e295fabd
Vitor Costa <vryzas27@gmail.com>
2022-06-17 12:31:18 +0100

    CA-8 - changed statIncrement method

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -2,16 +2,17 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const Match = require('./../models/matchModel');
 const User = require(`./../models/userModel`);
+const { Op } = require('sequelize');
 const sequelize = require('../utils/dbconnection');
 const lodash = require('lodash');
 
 // receives the array of matches a user has played
-const getTotalScore = (games, user) => {
+const getTotalScore = (matches, user) => {
   // lodash.sum computes the sum of the values of an array
   const score = lodash.sum(
     // array.map returns an array of values wich fulfil the condition
     // without changing the original
-    games.map((val) => {
+    matches.map((val) => {
       return user === val.username1 ? val.score1 : val.score2;
     })
   );
@@ -19,10 +20,8 @@ const getTotalScore = (games, user) => {
 };
 
 // increments the 'column' (draws, victories or defeats) for a given 'player'
-const statIncrement = async function (player, column) {
-  await sequelize.query(`UPDATE Stats
-  SET ${column}= ${column}+1
-  WHERE username='${player}'`);
+const statIncrement = async function (username, columnName) {
+  await sequelize.query(`UPDATE Stats SET ${columnName} = ${columnName}+1 WHERE username='${username}'`);
 };
 
 exports.playerMatches = catchAsync(async function (req, res, next) {
@@ -31,20 +30,20 @@ exports.playerMatches = catchAsync(async function (req, res, next) {
     return next(new AppError(`No player with that username found!`, 400));
   }
   // returns the array of matches played by the given player
-  const playerGames = await Match.findAll({
+  const playerMatches = await Match.findAll({
     where: {
       [Op.or]: [{ username1: req.params.username }, { username2: req.params.username }],
     },
   });
   return res.status(200).json({
-    message: `Player ${player.username} played ${playerGames.length} matches.`,
-    data: playerGames,
+    message: `Player ${player.username} played ${playerMatches.length} matches.`,
+    data: playerMatches,
   });
 });
 
 exports.compareMatches = catchAsync(async function (req, res, next) {
-  //returns an array with the matches featuring only the given players
-  const gamesUsers = await Match.findAll({
+  // returns an array with the matches featuring only the given players
+  const playersMatches = await Match.findAll({
     // finds games where username1 vs username2 OR username2 vs username1
     where: {
       [Op.or]: [
@@ -53,17 +52,17 @@ exports.compareMatches = catchAsync(async function (req, res, next) {
       ],
     },
   });
-  if (gamesUsers.length === 0) {
+  if (playersMatches.length == 0) {
     return res.status(404).json({ message: `No games found between these players!` });
   }
   // will get the total score of each player
-  const user1Score = getTotalScore(gamesUsers, req.body.username1);
-  const user2Score = getTotalScore(gamesUsers, req.body.username2);
+  const user1Score = getTotalScore(playersMatches, req.body.username1);
+  const user2Score = getTotalScore(playersMatches, req.body.username2);
 
   return res.status(200).json({
-    message: `${req.body.username1} and ${req.body.username2} played a total of ${gamesUsers.length} matches.
+    message: `${req.body.username1} and ${req.body.username2} played a total of ${playersMatches.length} matches.
       ${req.body.username1} scored a total of ${user1Score} points and ${req.body.username2} scored a total of ${user2Score} points.`,
-    data: gamesUsers,
+    data: playersMatches,
   });
 });
 
