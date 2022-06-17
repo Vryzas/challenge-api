8b0ce0eeb3825384ee72973468e470a30242b565
Vitor Costa <vryzas27@gmail.com>
2022-06-17 11:11:38 +0100

    CA-6 - removed message from catchAsync module

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -3,7 +3,6 @@ const errorController = require('./../controllers/errorController');
 module.exports = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((error) => {
-      error.status = 'FATAL ERROR';
       errorController(error, req, res, next);
     });
   };
