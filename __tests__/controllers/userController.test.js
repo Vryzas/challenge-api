831ecec81ce96f1772f3b9d5dc84d5c1cdc47aaa
Vitor Costa <vryzas27@gmail.com>
2022-06-21 10:10:23 +0100

    CA-9 - added getMe tests

--- __tests__/controllers/userController.test.js
+++ __tests__/controllers/userController.test.js
@@ -16,3 +16,41 @@ afterEach(() => {
   // clear the responses after each test (else they accumulate)
   clearMockRes();
 });
+describe('getMe', () => {
+  test('Invalid username.', async () => {
+    // arrange
+    const req = getMockReq({ query: { username: 'wrongUsername' } });
+    dao.profile.mockReturnValue(undefined);
+    // act
+    await userController.getMe(req, res, next);
+    // assert
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: `Failed to get ${req.query.username} profile data!` });
+  });
+
+  test('Valid username.', async () => {
+    // arrange
+    const req = getMockReq({ query: { username: 'username' } });
+    dao.profile.mockReturnValue({
+      username: 'username',
+      email: 'email@email.com',
+      victories: 0,
+      draws: 0,
+      defeats: 0,
+    });
+    // act
+    await userController.getMe(req, res, next);
+    // assert
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Success.',
+      data: {
+        username: 'username',
+        email: 'email@email.com',
+        victories: 0,
+        draws: 0,
+        defeats: 0,
+      },
+    });
+  });
+});
