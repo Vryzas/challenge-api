cbffc22c70e2c0c03f22059c6b07fb3266a0204f
Vitor Costa <vryzas27@gmail.com>
2022-06-23 16:57:09 +0100

    CA-10 - added leaderboard route/endpoint

--- routes/userStatsRoutes.js
+++ routes/userStatsRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/player/:username?', statsController.findStats);
 router.get('/compare', statsController.compareStats);
+router.get('/leaderboard', statsController.leaderboard);
 
 module.exports = router;
